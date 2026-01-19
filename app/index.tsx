aa8454306904052cf309671d8b46bc63c351ed0a
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2026-01-19 10:08:44 +0200

    Extracted the components

--- app/index.tsx
+++ app/index.tsx
@@ -16,20 +16,124 @@ import {
 import { Mood } from "@/types/supabase";
 import { formatDate } from "@/utils/date";
 import { sendMagicLink } from "@/api/auth";
+import { Session } from "@supabase/supabase-js";
+import { supabase } from "@/supabase";
+
+function LoginSection({
+  email,
+  setEmail,
+}: {
+  email: string;
+  setEmail: (v: string) => void;
+}) {
+  return (
+    <View style={{ justifyContent: "center" }}>
+      <Text style={{ marginBottom: 8, alignSelf: "center" }}>
+        Login with email
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="your@email.example"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+      <Button
+        title="Send magic link"
+        onPress={async () => {
+          try {
+            await sendMagicLink(email);
+            alert("Check your emails");
+          } catch (error) {
+            alert("Error while sending link");
+            console.log(error);
+          }
+        }}
+      />
+    </View>
+  );
+}
+
+function MoodSection({
+  mood,
+  setMood,
+  latestMood,
+  allMoods,
+  onSave,
+  onDelete,
+}: {
+  mood: string;
+  setMood: (v: string) => void;
+  latestMood: Mood | null;
+  allMoods: Mood[];
+  onSave: () => Promise<void>;
+  onDelete: () => Promise<void>;
+}) {
+  return (
+    <>
+      <View style={{ justifyContent: "center", marginBottom: 20 }}>
+        <Text style={styles.title}>How are you feeling today?</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Write your mood..."
+          value={mood}
+          onChangeText={setMood}
+        />
+        <Button title="Save Mood" onPress={onSave} />
+        <Button title="Delete Latest Mood" onPress={onDelete} />
+      </View>
+      {latestMood && (
+        <Text style={{ marginBottom: 20 }}>
+          Last Mood {latestMood.text} {"\n"} At:{" "}
+          {formatDate(latestMood.created_at)}
+        </Text>
+      )}
+
+      <Text style={{ fontWeight: "bold" }}>All Moods</Text>
+      <FlatList
+        data={allMoods}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ padding: 10 }}
+        renderItem={({ item }) => (
+          <Text style={{ marginVertical: 4 }}>
+            • {item.text} - {formatDate(item.created_at)}
+          </Text>
+        )}
+      />
+    </>
+  );
+}
 
 const App = () => {
   const [mood, setMood] = useState("");
   const [latestMood, setLatestMood] = useState<Mood | null>(null);
   const [allMoods, setAllMoods] = useState<Mood[]>([]);
   const [email, setEmail] = useState("");
+  const [session, setSession] = useState<Session | null>(null);
 
   // Load latest mood when app opens
 
   useEffect(() => {
     loadLatestMood();
     loadAllMoods();
+    getSession();
   }, []);
 
+  async function getSession() {
+    supabase.auth.getSession().then(({ data }) => {
+      setSession(data.session);
+    });
+
+    //Listen for future auth changes
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }
   async function loadAllMoods() {
     try {
       const data = await getAllMoods();
@@ -77,61 +181,18 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View style={{ marginBottom: 20, marginTop: "25%" }}>
-        <Text style={{ marginBottom: 10, alignSelf: "center" }}>
-          Login With Email
-        </Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Your@email.example"
-          value={email}
-          onChangeText={setEmail}
+      {!session ? (
+        <LoginSection email={email} setEmail={setEmail} />
+      ) : (
+        <MoodSection
+          mood={mood}
+          setMood={setMood}
+          latestMood={latestMood}
+          allMoods={allMoods}
+          onSave={saveMood}
+          onDelete={handleDeleteMood}
         />
-
-        <Button
-          title="Send login link"
-          onPress={async () => {
-            try {
-              await sendMagicLink(email);
-              alert("Check your emails for a login link");
-            } catch (err) {
-              alert("Error while sending login link");
-              console.log(err);
-            }
-          }}
-        />
-      </View>
-      <View style={{ marginTop: "10%" }}>
-        <Text style={styles.title}>How are you feeling today?</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Write your mood ..."
-          value={mood}
-          onChangeText={setMood}
-        />
-
-        <Button title="Save Mood" onPress={saveMood} />
-
-        {latestMood && (
-          <View>
-            <Text style={{ marginTop: 20 }}>
-              Last Mood: {latestMood.text} {"\n"} At:{" "}
-              {formatDate(latestMood.created_at)}
-            </Text>
-            <Button title="Delete Last Mood" onPress={handleDeleteMood} />
-          </View>
-        )}
-      </View>
-      <Text style={{ marginTop: 20, fontWeight: "bold" }}>All Moods</Text>
-      <FlatList
-        data={allMoods}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Text style={{ marginVertical: 4 }}>
-            • {item.text} - {formatDate(item.created_at)}
-          </Text>
-        )}
-      />
+      )}
     </View>
   );
 };
@@ -140,6 +201,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    marginTop: "50%",
   },
   title: {
     fontSize: 18,
