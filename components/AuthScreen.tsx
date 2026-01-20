5da407a93582f0e4793dedcbc3af8685d12cfadd
Lucky Nkolonzi <nkolonzilucky@gmail.com>
2026-01-20 19:36:30 +0200

    Added an AuthScreen component

---
+++ components/AuthScreen.tsx
@@ -0,0 +1,72 @@
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { supabase } from '@/supabase'
+
+const AuthScreen = ({ onAuthSuccess }: { onAuthSuccess: () => void }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+
+    async function signIn() {
+        setLoading(true)
+        setError(null)
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+
+        setLoading(false)
+
+        if (error) {
+            setError(error.message)            
+        } else {
+            onAuthSuccess()
+        }
+    }
+
+    async function signUp() {
+        setLoading(true)
+        setError(null)
+
+        const { error } = await supabase.auth.signUp({
+            email,
+            password
+        })
+
+        setLoading(false)
+
+        if (error) {
+            setError(error.message)
+        } else {
+            onAuthSuccess()
+        }
+
+    }
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', padding:20}}>
+          <Text style={{ fontSize: 22, marginBottom: 16 }}>Mood Tracker Login</Text>
+          <TextInput placeholder='Email' value={email} onChangeText={setEmail} autoCapitalize='none' style={styles.input} />
+          <TextInput placeholder='Password' value={password} onChangeText={setPassword} secureTextEntry style={styles.input} />
+
+          {error && (<Text style={{ color: 'red', marginBottom: 8 }}>{error}</Text>)}
+          
+          <Button title={loading ? 'Signing in...' : 'Sign In'} onPress={signIn} />
+          <Button title='Create Account' onPress={signUp} />
+          
+    </View>
+  )
+}
+
+export default AuthScreen
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 16,
+    width: "90%",
+    height: 40,
+  },
+});
